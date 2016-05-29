fc43c095a1d2a4f185ef6ad2248022a5ae502ec7
pgr <340227379@qq.com>
2016-05-29 21:09:21 +0800

    科目管理

---
+++ src/sei/tk/service/course/Course.java
@@ -0,0 +1,17 @@
+package sei.tk.service.course;
+
+import sei.tk.service.dao.model.TkCourse;
+import sei.tk.service.dao.model.vo.course.CourseVo;
+import sei.tk.util.Page;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on 2016/5/29.
+ */
+public interface Course {
+    public CourseVo getTkCourse(Short id);
+    public Page CourseList(Integer currentPage, Integer rows, CourseVo courseVo);
+    public boolean delete(TkCourse tkCourse);
+
+}
